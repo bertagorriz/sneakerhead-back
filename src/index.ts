2a143b9fadb1b9bd2b803e118ddda71a37897255
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-25 21:24:36 +0200

    Add mongodbConnection to database and debug responses

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import "./loadEnvironment.js";
 import createDebug from "debug";
 import chalk from "chalk";
 import { app } from "./server/app.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const debug = createDebug("sneakers-api:root");
 
 const port = process.env.PORT ?? 4000;
+const mongodbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongodbConnection) {
+  debug(chalk.red(`Missing environment variables`));
+  process.exit(1);
+}
 
 app.listen(port, () => {
   debug(chalk.greenBright(`Listening on port ${port}`));
 });
+
+try {
+  await connectToDatabase(mongodbConnection);
+} catch (error: unknown) {
+  debug(
+    `Error connecting to database ${chalk.redBright((error as Error).message)}`
+  );
+}
