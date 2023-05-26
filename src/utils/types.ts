8f9fc3ecafeda6e22881f5d83016223325119fd3
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-05-26 22:26:03 +0200

    Create getSneakers controller and required types and add it to app

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,4 +3,5 @@ import type CustomError from "../CustomError/CustomError.js";
 export interface ErrorStructure {
   wrongCredentials: CustomError;
   endpointNotFound: CustomError;
+  serverError: Error;
 }
