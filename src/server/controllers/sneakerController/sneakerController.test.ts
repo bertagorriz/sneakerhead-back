c19fe5990a2c2e1a0b32fc8d6e014cc09726b0bc
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-06-09 20:26:49 +0200

    Test addSneakers controller

--- src/server/controllers/sneakerController/sneakerController.test.ts
+++ src/server/controllers/sneakerController/sneakerController.test.ts
@@ -1,14 +1,25 @@
 import { type NextFunction, type Request, type Response } from "express";
 import Sneaker from "../../../database/models/Sneaker.js";
 import { mockSneakers } from "../../../mocks/sneakerMocks.js";
-import { deleteSneakers, getSneakers } from "./sneakerController.js";
+import {
+  addSneakers,
+  deleteSneakers,
+  getSneakers,
+} from "./sneakerController.js";
 import { responseErrorData } from "../../../utils/responseData/responseErrorData.js";
 import { type CustomRequest } from "../../../types.js";
 import CustomError from "../../../CustomError/CustomError.js";
+import { getSneakersDataMock } from "../../../mocks/factories/sneakersFactory.js";
+import { Types } from "mongoose";
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
+
+const errorStatus = 401;
+const errorMessage = "Sneaker not found";
+
+const sneakerToAdd = getSneakersDataMock(1);
 
 describe("Given a getSenakers controller", () => {
   const req = {};
@@ -111,7 +122,7 @@ describe("Given a deleteSneakers controller", () => {
 
       const next = jest.fn();
 
-      const error = new CustomError(404, "Sneaker not found");
+      const error = new CustomError(errorStatus, errorMessage);
 
       Sneaker.findByIdAndDelete = jest.fn().mockReturnValue({
         exec: jest.fn().mockResolvedValue(null),
@@ -119,6 +130,64 @@ describe("Given a deleteSneakers controller", () => {
 
       await deleteSneakers(
         req as CustomRequest<{ id: string }>,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given an addSneakers controller", () => {
+  const id = new Types.ObjectId().toString();
+
+  const req: Partial<CustomRequest> = {
+    body: sneakerToAdd,
+    userId: id,
+  };
+
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  const next = jest.fn();
+
+  describe("When it receives a request with a new sneaker data to add", () => {
+    Sneaker.create = jest.fn().mockResolvedValue(sneakerToAdd);
+
+    test("Then it should call the response's method status with status 201", async () => {
+      const expectedStatusCode = 201;
+
+      await addSneakers(
+        req as CustomRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call the response's method json with the new sneaker", async () => {
+      await addSneakers(
+        req as CustomRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ newSneaker: sneakerToAdd });
+    });
+  });
+
+  describe("When it receives a request with an invalid new sneaker to add and a next function", () => {
+    test("Then it should call the next function with the message 'Sneaker not found'", async () => {
+      const error = new CustomError(errorStatus, errorMessage);
+
+      Sneaker.create = jest.fn().mockResolvedValue(null);
+
+      await addSneakers(
+        req as CustomRequest,
         res as Response,
         next as NextFunction
       );
